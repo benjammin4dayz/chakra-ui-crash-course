189aa1055021fe83e65ee4fca6ef0fa11288f1b9
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-18 18:01:21 -0500

    feat: complete lesson 2
    https://www.youtube.com/watch?v=TswL6KhQDn4

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,35 @@
+import { Box, Container, Heading, Text } from '@chakra-ui/react';
+
 export default function Dashboard() {
+  const boxStyles = {
+    p: '10px',
+    bg: 'purple.400',
+    color: 'white',
+    m: '10px',
+    textAlign: 'center',
+    filter: 'blur(2px)',
+    '&:hover': {
+      filter: 'blur(0px)',
+      color: 'black',
+      bg: 'blue.200',
+    },
+  };
+
   return (
-    <div>Dashboard</div>
-  )
+    <Container as="section" maxWidth="4xl" py="20px">
+      <Heading my="30px" p="10px">
+        Chakra UI Components
+      </Heading>
+      <Text marginLeft="30px" color="blue.950">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+        laudantium?
+      </Text>
+
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is a box</Text>
+      </Box>
+
+      <Box css={boxStyles}>Hello, world!</Box>
+    </Container>
+  );
 }
