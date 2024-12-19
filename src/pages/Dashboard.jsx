9ab40aaf514686105b2b1c85687c3455e3cf55f7
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-18 20:23:43 -0500

    feat: complete lesson 7
    https://www.youtube.com/watch?v=pExAE289j9s

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,67 @@
-import { Box, Container, Heading, SimpleGrid, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Separator,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import { useLoaderData } from 'react-router-dom';
+import { FaEye, FaEdit } from 'react-icons/fa';
 
 export default function Dashboard() {
+  const tasks = useLoaderData();
+
   return (
-    <SimpleGrid p="10px" gap={10} minChildWidth="250px">
-      <Box bg="white" h="200px" border="1px solid">
-        <Text color={{ base: 'pink', md: 'blue', lg: 'green' }}>Hello</Text>
-      </Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
-      <Box bg="white" h="200px" border="1px solid"></Box>
+    <SimpleGrid gap={10} minChildWidth="300px">
+      {tasks &&
+        tasks.map(task => (
+          <Card.Root
+            key={task.id}
+            borderTop="8px"
+            borderColor="purple.400"
+            bg="white"
+          >
+            <Card.Header>
+              <Flex gap={3}>
+                <Box w="50px" h="50px">
+                  <Text>AV</Text>
+                </Box>
+                <Box>
+                  <Heading as="h3" size="sm">
+                    {task.title}
+                  </Heading>
+                  <Text>by {task.author}</Text>
+                </Box>
+              </Flex>
+            </Card.Header>
+            <Card.Body color="gray.500">
+              <Text>{task.description}</Text>
+            </Card.Body>
+            <Separator borderColor="gray.200" />
+            <Card.Footer>
+              <HStack>
+                <Button variant="ghost">
+                  <FaEye />
+                  Watch
+                </Button>
+                <Button variant="ghost">
+                  <FaEdit />
+                  Comment
+                </Button>
+              </HStack>
+            </Card.Footer>
+          </Card.Root>
+        ))}
     </SimpleGrid>
   );
 }
+
+export const tasksLoader = async () => {
+  const res = await fetch('http://localhost:5000/tasks');
+  return res.ok ? res.json() : [];
+};
