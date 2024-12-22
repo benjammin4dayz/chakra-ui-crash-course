68ee7f38953379f784637cef574947f21330a0cc
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-22 06:39:55 -0500

    feat: complete lesson 10
    https://www.youtube.com/watch?v=AsbBEGhbaOg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,19 @@ import {
   Spacer,
   Text,
 } from '@chakra-ui/react';
+import { Toaster, toaster } from '../components/ui/toaster';
 
 export const Navbar = () => {
+  const showToast = () => {
+    toaster.create({
+      title: 'Logged out',
+      description: 'Successfully logged out',
+      duration: 5000,
+      type: 'success',
+      meta: { closable: true },
+    });
+  };
+
   return (
     <Flex as="nav" p="10px" mb="40px" alignItems="center" gap="10px">
       <Heading as="h1">Dojo Tasks</Heading>
@@ -19,8 +30,9 @@ export const Navbar = () => {
           M
         </Box>
         <Text>mario@netninja.dev</Text>
-        <Button>Logout</Button>
+        <Button onClick={showToast}>Logout</Button>
       </HStack>
+      <Toaster />
     </Flex>
   );
 };
