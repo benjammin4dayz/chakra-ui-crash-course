8f81262c03c0ca6a83c722277c60fbc282e11f87
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-22 07:04:07 -0500

    feat: complete lesson 12
    https://www.youtube.com/watch?v=FKuNkIFlgdU

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
+
+export const customSystem = createSystem(defaultConfig, {
+  theme: {
+    tokens: {
+      fonts: {
+        heading: { value: `Courier New` },
+        body: { value: `Tahoma` },
+      },
+      colors: {
+        brand: {
+          900: '#024fc9',
+          800: '#146af5',
+          700: '#297742',
+          600: '#337df2',
+          500: '#4287f5',
+        },
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={customSystem}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
